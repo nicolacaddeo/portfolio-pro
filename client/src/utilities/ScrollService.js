c54f38207fc7c1de84040caf79279720a92b9997
Nicola Caddeo <85455970+nicolacaddeo@users.noreply.github.com>
2023-03-29 22:04:13 +0200

    added header component

--- client/src/utilities/ScrollService.js
+++ client/src/utilities/ScrollService.js
@@ -44,7 +44,7 @@ export default class ScrollService {
   };
 
   checkCurrentScreenUnderViewport = (event) => {
-    if (!event || object.keys(event).lenght < 1) return;
+    if (!event || Object.keys(event).lenght < 1) return;
     for (let screen of TOTAL_SCREENS) {
       let screenFromDOM = document.getElementById(screen.screen_name);
       if (!screenFromDOM) continue;
