7018960d0dd4c7d366b09f05592659720f818aa9
Nicola Caddeo <85455970+nicolacaddeo@users.noreply.github.com>
2023-03-29 10:27:04 +0200

    created profile component
    added social media icons

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Profile from './PortofolioContainer/Home/Profile';
 
 function App() {
   return (
     <div className="App">
-
+      <Profile/>
     </div>
   );
 }
---
+++ client/src/PortofolioContainer/Home/Profile.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Profile() {
+    return (
+        <div className='profile-container'>
+            <div className='profile-parent'>
+                <div className='profile-details'>
+                    <div className='colz'>
+                        <a href="https://www.linkedin.com/in/nicola-giovanni-caddeo-653314259/">
+                            <i className='fa fa-linkedin-square'></i>
+                        </a>
+                        <a href="https://github.com/nicolacaddeo">
+                            <i className='fa fa-github-square'></i>
+                        </a>
+                        <a href="https://www.instagram.com/nicolacaddeoo/">
+                            <i className='fa fa-instagram'></i>
+                        </a>
+                        <a href="https://www.youtube.com/">
+                            <i className='fa fa-youtube-square'></i>
+                        </a>
+                        <a href="https://www.twitter.com/">
+                            <i className='fa fa-twitter'></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
