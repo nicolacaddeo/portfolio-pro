14faecf1535638f0c6f61000cb3315d4927ac419
Nicola Caddeo <85455970+nicolacaddeo@users.noreply.github.com>
2023-03-30 13:41:36 +0200

    added AboutMe component

--- client/src/utilities/commonUtils.js
+++ client/src/utilities/commonUtils.js
@@ -1,9 +1,14 @@
 import Home from "../PortofolioContainer/Home/Home";
+import AboutMe from "../PortofolioContainer/AboutMe/AboutMe";
 
 export const TOTAL_SCREENS = [
   {
     screen_name: "Home",
     component: Home,
+  },
+  {
+    screen_name: "AboutMe",
+    component: AboutMe,
   },
 ];
 export const GET_SCREEN_INDEX = (screen_name) => {
