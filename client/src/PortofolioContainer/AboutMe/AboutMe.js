2b5ff6856fd38cb94fd4ebbe07b3fa55555fcd4d
Nicola Caddeo <85455970+nicolacaddeo@users.noreply.github.com>
2023-03-31 02:39:26 +0200

    Update AboutMe.js

--- client/src/PortofolioContainer/AboutMe/AboutMe.js
+++ client/src/PortofolioContainer/AboutMe/AboutMe.js
@@ -1,12 +1,70 @@
-import React, {useEffect} from 'react';
-import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading'
-import ScrollService from '../../utilities/ScrollService'
-import Animations from '../../utilities/Animation'
+import React, { useEffect } from "react";
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animation";
 
-export default function AboutMe() {
+export default function AboutMe(props) {
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const SCREEN_CONSTSANTS = {
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    highlights: {
+      bullets: [
+        "Jr. Full Stack web developer",
+        "Paradigma OOP",
+        "Database relazionali",
+        "Sistemi di versionamento",
+        "Esperienza di progetti in team",
+      ],
+      heading: "Ecco alcuni punti di forza:",
+    },
+  };
+  const renderHighlight = () => {
+    return SCREEN_CONSTSANTS.highlights.bullets.map((value, i) => (
+      <div className="highlight" key={i}>
+        <div className="highlight-blob"></div>
+        <span>{value}</span>
+      </div>
+    ));
+  };
+
   return (
-    <div>
-
+    <div className="about-me-container screen-container" id={props.id || ""}>
+      <div className="about-me-parent">
+        <ScreenHeading title={"About Me"} subHeading={"Perché scegliermi?"} />
+        <div className="about-me-card">
+          <div className="about-me-profile"></div>
+          <div className="about-me-details">
+            <span className="about-me-description">
+              {SCREEN_CONSTSANTS.description}
+            </span>
+            <div className="about-me-highlights">
+              <div className="highlights-heading">
+                <span>{SCREEN_CONSTSANTS.highlights.heading}</span>
+              </div>
+              {renderHighlight()}
+            </div>
+            <div className="about-me-options">
+            <button className="btn primary-btn">
+              {""}
+              Hire Me{" "}
+            </button>
+            <a
+              href="nicola_giovanni_caddeo_cv.pdf"
+              download="Nicola Giovanni Caddeo cv.pdf"
+            >
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
