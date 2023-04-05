d33615d26f2fd58954f3256286ea1c1c5f20df96
Nicola Caddeo <85455970+nicolacaddeo@users.noreply.github.com>
2023-04-05 19:22:44 +0200

    fixed scrollToHireMe() function

--- client/src/utilities/ScrollService.js
+++ client/src/utilities/ScrollService.js
@@ -11,7 +11,7 @@ export default class ScrollService {
     window.addEventListener("scroll", this.checkCurrentScreenUnderViewport);
   }
   scrollToHireMe = () => {
-    let contactMeScreen = document.getElementById("ContactMe");
+    let contactMeScreen = document.getElementById("Contattami");
     if (!contactMeScreen) return;
     contactMeScreen.scrollIntoView({ behavior: "smooth" });
   };
