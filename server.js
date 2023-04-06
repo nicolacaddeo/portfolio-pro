14f94bff7fa5412d5c87a2a5aa7236b8c3384426
Nicola Caddeo <85455970+nicolacaddeo@users.noreply.github.com>
2023-04-06 14:32:55 +0200

    added code for deployment on heroku

--- server.js
+++ server.js
@@ -12,5 +12,13 @@ app.use(cors());
 
 app.use("/", contactRoute);
 
+// deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(`server listing to port 5000 only`));
